2bf00457b7161c2a88af443ddaa031569a06351f
anotherso1a <1181581742@qq.com>
2019-10-22 13:33:54 +0800

    fix(单元测试): 调整节流函数的测试逻辑

--- __tests__/function/trodeb.test.ts
+++ __tests__/function/trodeb.test.ts
@@ -2,19 +2,20 @@ import { throttle } from '../../src/function/throttle';
 // import {  debounce} from "../../src/function/debounce";
 
 test('throttle', () => {
-  let count: Number = 0;
-  let fn = n => n++;
+  let count1: Number = 0;
+  let count2: Number = 0;
+  let fn:Function = n => n++;
   let throttled = throttle(fn, 20);
   for (let i = 0; i < 5; i++) {
     setTimeout(() => {
-      throttled(count);
-      expect(count).toBe(i + 1);
+      throttled(count1);
+      expect(count1).toBe(i + 1);
     }, i * 21);
   }
   for (let i = 0; i < 2; i++) {
     setTimeout(() => {
-      throttled(count);
-      expect(count).toBe(1);
+      throttled(count2);
+      expect(count2).toBe(1);
     }, i * 9);
   }
 });
