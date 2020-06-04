0ee06cd5f76f074f02698a5800b7f1d7a3ea4d06
anotherso1a <xiezhipeng@didiglobal.com>
2020-06-04 12:04:43 +0800

    feat(0.0.8): add floatCompare function

--- __tests__/object/deepClone.test.ts
+++ __tests__/object/deepClone.test.ts
@@ -1,13 +1,11 @@
 import { deepClone } from '../../src/object/deepClone';
 
 test('deepClone', () => {
-
-  let a : any = { a:"A",b:[1,2,3,4,5] };
+  let a: any = { a: 'A', b: [1, 2, 3, 4, 5] };
   a.c = a;
-  let copy : any = deepClone(a);
+  let copy: any = deepClone(a);
 
-  expect(copy===a).toBe(false);
-  expect(copy.a===a.a).toBe(true);
-  expect(copy.b===a.b).toBe(false);
-
+  expect(copy === a).toBe(false);
+  expect(copy.a === a.a).toBe(true);
+  expect(copy.b === a.b).toBe(false);
 });
