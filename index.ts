78675956d1e66a7244474ee7f813625d99657cce
anotherso1a <1181581742@qq.com>
2020-07-29 13:58:57 +0800

    refactor: add export default

--- index.ts
+++ index.ts
@@ -1,4 +1,16 @@
-export * from './src/array'
+import * as arrayFn from './src/array'
+import * as functionalFn from './src/function'
+import * as objectFn from './src/object'
+import * as stringFn from './src/string'
+
 export * from './src/function'
 export * from './src/object'
 export * from './src/string'
+export * from './src/array'
+
+export default {
+  ...arrayFn,
+  ...functionalFn,
+  ...objectFn,
+  ...stringFn
+}
