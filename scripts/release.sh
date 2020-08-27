e0861e5670b902e954fa459066388402a7cc6f01
anotherso1a <1181581742@qq.com>
2020-08-27 11:42:47 +0800

    ci: test release

---
+++ scripts/release.sh
@@ -0,0 +1,14 @@
+BRANCH=`git branch -a|grep '.*\*'|sed 's/[*[:space:]]*//g'`
+STATUS=`git status --porcelain`
+
+if [ "$STATUS" ]; then
+  echo "please claer your working-tree first!\n"
+  exit 1
+fi
+
+if [ $BRANCH != "master" ]; then
+  echo "please run release on master branch!\n"
+  exit 1
+fi
+
+npm version patch -m '[release] patch: @%s'
