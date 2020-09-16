af98149f1e7193fd1638515bbc68004c02ed778a
anotherso1a <1181581742@qq.com>
2020-09-16 15:25:30 +0800

    ci: fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,9 @@ git add dist/
 
 BUILD_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 
-git commit -m "build: automatic build\nbuild time: $BUILD_TIME"
+STATUS=`git status --porcelain`
 
-git push origin master
+if [ "$STATUS" ]; then
+  git commit -m "build: automatic build\nbuild time: $BUILD_TIME"
+  git push origin master
+fi
